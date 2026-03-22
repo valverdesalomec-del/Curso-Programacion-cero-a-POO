1b9f37580f77bce11d605b746b2a42228c847bdf
Carla Curay <valverde.salomec@gmail.com>
2026-03-21 19:49:40 -0500

    Mostrar estado del usuario: Aprobado o Reprobado

--- clase11/ejercicioDePrueba.cpp
+++ clase11/ejercicioDePrueba.cpp
@@ -8,31 +8,57 @@ calcular el promedio final y mostrar si el estudiante aprueba o reprueba.
 */
 #include <iostream>
 using namespace std;
+
 int main()
 {
+    // Variables para almacenar las 3 notas y el promedio
     double n1, n2, n3, promedio;
-    cout << "=== SISTEMA SIMPLE DE PROMEDIO ===" << endl;
+
+    cout << "==========================" << endl;
+    cout << "SISTEMA SIMPLE DE PROMEDIO" << endl;
+    cout << "==========================" << endl;
+
+    // Ingreso y validacion de la primera nota
     cout << "Ingrese la nota 1: ";
     cin >> n1;
-    while(n1 < 0 || n1 > 10){
-        cout << "ERROR. Ingrese una nota entre 0 y 10: "; 
+    while (n1 < 0 || n1 > 10)
+    {
+        cout << "Error. Ingrese una nota entre 0 y 10: ";
         cin >> n1;
     }
 
+    // Ingreso y validacion de la segunda nota
     cout << "Ingrese la nota 2: ";
     cin >> n2;
-    while(n2 < 0 || n2 > 10){
-        cout << "ERROR. Ingrese una nota entre 0 y 10: "; 
+    while (n2 < 0 || n2 > 10)
+    {
+        cout << "Error. Ingrese una nota entre 0 y 10: ";
         cin >> n2;
     }
 
+    // Ingreso y validacion de la tercera nota
     cout << "Ingrese la nota 3: ";
     cin >> n3;
-    while(n3 < 0 || n3 > 10){
-        cout << "ERROR. Ingrese una nota entre 0 y 10: "; 
+    while (n3 < 0 || n3 > 10)
+    {
+        cout << "Error. Ingrese una nota entre 0 y 10: ";
         cin >> n3;
     }
+
+    // calcular el promedio
     promedio = (n1 + n2 + n3) / 3;
+
     cout << "Promedio final: " << promedio << endl;
+
+    // Evaluacion del estado final
+    if (promedio >= 7)
+    {
+        cout << "Estado: APROBADO" << endl;
+    }
+    else
+    {
+        cout << "Estado: REPROBADO" << endl;
+    }
+
     return 0;
 }
